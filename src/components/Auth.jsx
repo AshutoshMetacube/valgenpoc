424ae1b270e06302f6c11e6b38f676a7a3053512
ASHUTOSH-TRIPAT\Ashutosh <ashutosh.tripathi@metacube.com>
2023-02-15 17:21:39 +0530

    Integrated changes

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -2,30 +2,33 @@ import React, { useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import axios from "axios";
 import qs from "qs";
-//import LoadingOverlay from "react-loading-overlay";
 import Loader from "./Loader";
 
 const Auth = () => {
-  const referrer = document.referrer;
-  const [searchParams] = useSearchParams();
-
-  const [code, setCode] = useState("");
   const navigate = useNavigate();
-  
+  const [searchParams] = useSearchParams();
+  const [code, setCode] = useState("");
   const search = window.location.search;
   const params = new URLSearchParams(search);
   const codeText = params.get("code");
-
+  /**
+   * get code from params, decode it and hit api for get access token object
+   */
   useEffect(() => {
+    /*Decode code uri */
     setCode(decodeURIComponent(searchParams.get("code")));
+    /**
+     * create a request for access token
+     */
     const getToken = async () => {
       const baseURL = `${document.referrer}services/oauth2/token`;
       let body = {
         grant_type: "authorization_code",
-        code:codeText,
+        code: codeText,
         client_id: process.env.REACT_APP_CLIENT_ID,
         client_secret: process.env.REACT_APP_CLIENT_SECRET,
-        redirect_uri: 'https://ashutoshmetacube.github.io/velgenpoc/#/auth',
+        redirect_uri: "https://ashutoshmetacube.github.io/valgenpoc/#/auth",
+        //redirect_uri: decodeURI(process.env.REACT_APP_REDIRECT_URI),
       };
 
       const options = {
@@ -34,31 +37,28 @@ const Auth = () => {
         data: qs.stringify(body),
         url: baseURL,
       };
-
       await axios(options)
         .then((response) => {
-          console.log("response>>>>", response);
-          console.log("response.data>>>>", JSON.stringify(response.data));
-          console.log("response.data.access_token>>>>", JSON.stringify(response.data.access_token));
-          sessionStorage.setItem('data', JSON.stringify(response.data))
-          sessionStorage.setItem('token', response.data.access_token)
-          navigate('/csv')
+          // After getting a response from salesforce set data in our localStorage
+          sessionStorage.setItem("data", JSON.stringify(response.data));
+          sessionStorage.setItem("token", response.data.access_token);
+
+          //Navigate to CSV uri
+          navigate("/csv");
         })
         .catch((e) => {
-          // sessionStorage.setItem('data', JSON.stringify(sample))
-          // sessionStorage.setItem('token', sample.access_token)
-          // navigate('/csv')
-          console.log("e >>>", e);
+          console.log({ e });
         });
     };
+    /**
+     * Execute token function when code is getting from params
+     */
     if (code) {
       getToken();
     }
   }, [code, searchParams]);
 
-  return (
-    <Loader isActive={true} />
-  )
+  return <Loader isActive={true} />;
 };
 
 export default Auth;
